eddf41e3687240c0b8bf663d503c39c8043ee411
S88inua <s88inua@gmail.com>
2018-11-12 16:17:58 +0200

    Домашние задания №0

---
+++ examp0.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Me
+ */
+const TESTCONSTANTA = 'asd';
+$User_Name = "Igor";
+
+function show_something()
+{
+    return 'SOMETHING';
+}
+
+
+if (1 == 1) {
+    echo 'hi';
+}
